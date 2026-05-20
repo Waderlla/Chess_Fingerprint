aeea98da7781b6c38004eee03b52bafc1226d7fb
Olga Mirończuk <olga.mironczuk.data@gmail.com>
2026-05-20 18:25:00 +0000

    clean repo

--- run_fetch.py
+++
@@ -1,7 +0,0 @@
-from data_fetcher import main
-
-# Uruchamia miesięczny sync danych źródłowych:
-# - pobiera brakujące partie
-# - nie zapisuje duplikatów
-# - czyści stare partie poza oknem retencji
-main()
